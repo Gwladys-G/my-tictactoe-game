e7eff7c736d7d26fe99f20272ce6fee529055c50
Gwladys Engels <gwladys.engels@gmail.com>
2022-08-08 17:46:47 +0200

    added winning popups

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS =[
+  [0,1,3],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
 const cellElements = document.querySelectorAll("[data-cell")
 const board = document.getElementById("board")
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
+const winningMessageElem = document.getElementById("winningMessage")
 
 let circleTurn
 
@@ -13,8 +25,12 @@ cellElements.forEach(cell => {
 function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-  console.log(currentClass)
   placeMark(cell,currentClass)
+  if (checWin(currentClass)){
+    console.log("winner")
+    endGame(false)
+  }
+
   // Check for Win
   // Check for draw
   swapTurns()
@@ -37,4 +53,23 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS)
   }
+}
+
+
+function checWin(currentClass){
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+
+
+function endGame(draw) {
+  if (draw) {
+
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} wins!`
+  }
+  winningMessageElem.classList.add("show")
 }
